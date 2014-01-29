40e05b13a27e47defec59e63027ba241dbcd35b4
AbelOria <abel_oria@yahoo.es>
2014-01-29 00:33:49 -0500

    Update Mat.java
    master gitub j'aai efface presque tout

--- src/Mat.java
+++ src/Mat.java
@@ -3,26 +3,5 @@ public class Mat {
 	public void somme(){
 		
 	}
-	
-	public void soustaction(){
-		
-	}
-	
-	public void addition(){
-		int a = 10;
-	}
-	
-	
-	public void melamela(){
-		
-	}
-	
-	public void sun(){
-		//C'est tres interesant
-	}
-	
-	public void mon(){
-		
-		int b =20;
-	}
+
 }
